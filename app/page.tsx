c946d310f8ff1bc5abe33f3a07efb0fa65cd0282
L Bories <lucbories07@gmail.com>
2023-11-15 00:26:13 +0100

    fix google pageSpeed2

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,13 @@ const Home = async () => {
                 alt="arrow-right"
                 width={16}
                 height={16}
+                loading="eager"
               />
             </p>
 
             <h1 className="head-text">
               Unleash the Power of
-              <span className="text-primary">PriceWise</span>
+              <span className="text-primary"> PriceWise</span>
             </h1>
 
             <p className="mt-6">
